b2e238266728e387c70659948547a2dc7b40f98e
Jonas <163044645+PizzaTreeIsland@users.noreply.github.com>
2025-03-01 20:02:26 +0100

    Added files

---
+++ RedditUsernameBot.py
@@ -0,0 +1,39 @@
+import praw
+import datetime
+import re
+
+# Reddit API Credentials
+reddit = praw.Reddit(
+    client_id="CLIENT-ID",
+    client_secret="CLIENT-SECRET",
+    user_agent="UserNameCommenter v. 0.1 by u/UsernameCommenterBot",
+    username="UsernameCommenterBot",
+    password="PASSWORD"
+)
+
+# List of subreddits to monitor
+with open(r"subredditlist.txt", "r") as listfile:
+    subreddits_to_monitor = [line.strip() for line in listfile.readlines()]
+
+subreddits_to_monitor=subreddits_to_monitor[:600:] #rate limiting to avoid api errors. Can be resolved by parrallelizing.
+
+
+target_string = "u/"
+
+
+def post_comment(post):
+    usernames=re.findall(r"u/(\w+)", post.title)
+    usernames=[f"u/{name}" for name in usernames]
+
+    if len(usernames)==1:
+        post.reply(f"It seems you mentioned {usernames[0]} in your submission title. I mentioned them here so mobile users can easily click on the name. I am a bot. If I did something wrong, please message me. ")
+        print(f"{datetime.datetime.now()}: Found post {post.title} and commented username {usernames[0]}. {post.shortlink}")
+    else:
+        post.reply(f"It seems you mentioned the following users in your submission title: \n {', '.join(usernames)}\nI mentioned them here so mobile users can easily click on the name. I am a bot. If I did something wrong, please message me. ")
+        print(f"{datetime.datetime.now()}: Found post {post.title} and commented these usernames: {', '.join(usernames)}. {post.shortlink}")
+
+# Monitor new posts
+subreddit = reddit.subreddit("+".join(subreddits_to_monitor))
+for post in subreddit.stream.submissions(skip_existing=True):
+    if target_string.lower() in post.title.lower():
+        post_comment(post)
