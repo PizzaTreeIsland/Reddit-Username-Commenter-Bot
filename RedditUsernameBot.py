a149786633b4cbcfc2f576dce38cd370260ce0e2
Jonas <163044645+PizzaTreeIsland@users.noreply.github.com>
2025-03-02 10:55:24 +0100

    Several fixes
    - Added error handling for the api call
    - Updated regex to capture all non-whitespace characters in usernames
    - Handled case where username list is empty

--- RedditUsernameBot.py
+++ RedditUsernameBot.py
@@ -1,12 +1,13 @@
 import praw
 import datetime
 import re
+import time
 
 # Reddit API Credentials
 reddit = praw.Reddit(
     client_id="CLIENT-ID",
     client_secret="CLIENT-SECRET",
-    user_agent="UserNameCommenter v. 0.1 by u/UsernameCommenterBot",
+    user_agent="UserNameCommenter v. 0.2 by u/UsernameCommenterBot",
     username="UsernameCommenterBot",
     password="PASSWORD"
 )
@@ -22,18 +23,31 @@ target_string = "u/"
 
 
 def post_comment(post):
-    usernames=re.findall(r"u/(\w+)", post.title)
+    usernames=re.findall(r"u/(\S+)", post.title)
     usernames=[f"u/{name}" for name in usernames]
 
     if len(usernames)==1:
         post.reply(f"It seems you mentioned {usernames[0]} in your submission title. I mentioned them here so mobile users can easily click on the name. I am a bot. If I did something wrong, please message me. ")
         print(f"{datetime.datetime.now()}: Found post {post.title} and commented username {usernames[0]}. {post.shortlink}")
-    else:
+    elif len(usernames)>1:
         post.reply(f"It seems you mentioned the following users in your submission title: \n {', '.join(usernames)}\nI mentioned them here so mobile users can easily click on the name. I am a bot. If I did something wrong, please message me. ")
         print(f"{datetime.datetime.now()}: Found post {post.title} and commented these usernames: {', '.join(usernames)}. {post.shortlink}")
-
+    else: 
+        print(f"{datetime.datetime.now()}: Found post {post.title} but couldn't find an usernames. {post.shortlink}")
+        
 # Monitor new posts
 subreddit = reddit.subreddit("+".join(subreddits_to_monitor))
-for post in subreddit.stream.submissions(skip_existing=True):
-    if target_string.lower() in post.title.lower():
-        post_comment(post)
+def monitor():
+    global subreddit
+    global target_string
+    global reddit
+    try: 
+        for post in subreddit.stream.submissions(skip_existing=True):
+            if target_string.lower() in post.title.lower():
+                post_comment(post)
+    except: 
+        print("An error occured. Trying again in 10seconds")
+        time.sleep(10)
+        monitor()
+        
+monitor()
